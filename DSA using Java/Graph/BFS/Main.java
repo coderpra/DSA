728632408b31bef7c6107f064026534f822d0447
coderpra <pratikkumardewan@gmail.com>
2023-11-17 21:11:56 +0530

    f

--- DSA using Java/Graph/BFS/Main.java
+++ DSA using Java/Graph/BFS/Main.java
@@ -1,6 +1,29 @@
 /*Program to find Breath First Search(BFS) of an Undireced Graph */
 import java.util.*;
 public class Main {
+    private static ArrayList<Integer> BFS(ArrayList<ArrayList<Integer>> list){
+        int v = list.size(); //get the number of vertices present in the graph which is actually the size of the graph
+        Queue<Integer> queue = new LinkedList<>();
+        int visited [] = new int[v];
+        ArrayList<Integer> bfs = new ArrayList<>(v);
+
+        queue.add(1); //assuming the graph is connected with vertix 1
+        visited[0]=1;
+        bfs.add(1); // Add the first vertex to the BFS list
+        while(!queue.isEmpty()){
+            int node =queue.poll();
+
+            for(int neighbor : list.get(node-1)){ // Iterate through the neighbors of the current node
+                if(visited[neighbor-1]==1) continue;
+                queue.add(neighbor);
+                visited[neighbor-1]=1;
+                bfs.add(neighbor); // Add the neighbor to the BFS list
+
+            }
+        }
+        return bfs;
+    }
+
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of vertices and number of edges (separate them using a space like: V E): ");
@@ -30,25 +53,8 @@ public class Main {
                 list.get(e2 - 1).add(e1);
             }
         }
-        Queue<Integer> queue = new LinkedList<>();
-        int visited [] = new int[v];
-        ArrayList<Integer> bfs = new ArrayList<>(v);
-
-        queue.add(1); //assuming the graph is connected with vertix 1
-        visited[0]=1;
-        bfs.add(1); // Add the first vertex to the BFS list
-        while(!queue.isEmpty()){
-            int node =queue.poll();
-
-            for(int neighbor : list.get(node-1)){ // Iterate through the neighbors of the current node
-                if(visited[neighbor-1]==1) continue;
-                queue.add(neighbor);
-                visited[neighbor-1]=1;
-                bfs.add(neighbor); // Add the neighbor to the BFS list
-
-            }
-        }
-        System.out.println("BFS --> "+bfs); // Output the BFS result
+        
+        System.out.println("BFS --> "+BFS(list)); // Output the BFS result
         sc.close();
     }
 }
